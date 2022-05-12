eed22fbe51cda89f3499997f4bf29bf7b80dbbc7
Boy van der Laak <Hless@users.noreply.github.com>
2022-05-12 14:59:34 +0200

    test: fixed existing tests and added datocms test

--- gatsby-node.test.js
+++ gatsby-node.test.js
@@ -43,6 +43,18 @@ const contentfulAssetFixture = {
   updatedAt: '123'
 }
 
+const datoCmsAssetFixture = {
+  internal: {
+    contentDigest: '123'
+  },
+  entityPayload: {
+    attributes: {
+      url: 'https://localhost/mocked.svg',
+      mime_type: 'image/svg+xml'
+    }
+  }
+}
+
 describe('general', () => {
   beforeEach(() => {
     createResolversMock.mockClear()
@@ -50,17 +62,42 @@ describe('general', () => {
     reporter.panic.mockClear()
   })
 
-  test('create resolver', async () => {
+  test('create resolvers', async () => {
     createResolvers({
       cache,
       createResolvers: createResolversMock,
       store,
       reporter
     })
-    expect(registeredResolvers.size).toBe(2)
+    expect(registeredResolvers.size).toBe(3)
+  })
+
+  test('create contentful resolver', async () => {
+    createResolvers({
+      cache,
+      createResolvers: createResolversMock,
+      store,
+      reporter
+    })
+
     expect(registeredResolvers.has('ContentfulAsset')).toBe(true)
 
     const resolverData = registeredResolvers.get('ContentfulAsset')
+    expect(resolverData.svg).toBeTruthy()
+    expect(resolverData.svg.type).toBe('InlineSvg')
+  })
+
+  test('create datocms resolver', async () => {
+    createResolvers({
+      cache,
+      createResolvers: createResolversMock,
+      store,
+      reporter
+    })
+
+    expect(registeredResolvers.has('DatoCmsAsset')).toBe(true)
+
+    const resolverData = registeredResolvers.get('DatoCmsAsset')
     expect(resolverData.svg).toBeTruthy()
     expect(resolverData.svg.type).toBe('InlineSvg')
   })
@@ -205,6 +242,81 @@ describe('gatsby-source-contentful', () => {
 
     const noFileFixture = JSON.parse(JSON.stringify(contentfulAssetFixture))
     delete noFileFixture.file.url
+    const result = await resolverData.svg.resolve(noFileFixture)
+
+    expect(result).toBe(null)
+    expect(reporter.panic).not.toHaveBeenCalled()
+  })
+})
+
+describe('gatsby-source-datocms', () => {
+  beforeEach(() => {
+    createResolversMock.mockClear()
+    registeredResolvers.clear()
+    reporter.panic.mockClear()
+    nock.cleanAll()
+    cache.actualMap.clear()
+  })
+  test('process DatoCMS Asset', async () => {
+    createResolvers({
+      cache,
+      createResolvers: createResolversMock,
+      store,
+      reporter
+    })
+
+    nock('https://localhost')
+      .get(`/mocked.svg`)
+      .replyWithFile(200, fixturePath, {
+        'Content-Type': 'image/svg+xml'
+      })
+
+    const resolverData = registeredResolvers.get('DatoCmsAsset')
+    const result = await resolverData.svg.resolve(datoCmsAssetFixture)
+
+    expect(reporter.panic).not.toBeCalled()
+
+    expect(result.absolutePath).toContain('svg')
+    expect(result.absolutePath).toContain(__dirname)
+    expect(result.content).toBe(
+      `<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 28 28\"><circle cx=\"14\" cy=\"14\" r=\"14\" fill=\"#639\"/><path fill=\"#fff\" d=\"M6.2 21.8C4.1 19.7 3 16.9 3 14.2L13.9 25c-2.8-.1-5.6-1.1-7.7-3.2zm10.2 2.9L3.3 11.6C4.4 6.7 8.8 3 14 3c3.7 0 6.9 1.8 8.9 4.5l-1.5 1.3A9.23 9.23 0 0 0 14 5a9.1 9.1 0 0 0-8.5 6L17 22.5c2.9-1 5.1-3.5 5.8-6.5H18v-2h7c0 5.2-3.7 9.6-8.6 10.7z\"/></svg>`
+    )
+    expect(result.dataURI).toBe(
+      `data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 28 28'%3e%3ccircle cx='14' cy='14' r='14' fill='%23639'/%3e%3cpath fill='white' d='M6.2 21.8C4.1 19.7 3 16.9 3 14.2L13.9 25c-2.8-.1-5.6-1.1-7.7-3.2zm10.2 2.9L3.3 11.6C4.4 6.7 8.8 3 14 3c3.7 0 6.9 1.8 8.9 4.5l-1.5 1.3A9.23 9.23 0 0 0 14 5a9.1 9.1 0 0 0-8.5 6L17 22.5c2.9-1 5.1-3.5 5.8-6.5H18v-2h7c0 5.2-3.7 9.6-8.6 10.7z'/%3e%3c/svg%3e`
+    )
+    expect(result.originalContent).toBe(
+      fs.readFileSync(__dirname + '/fixtures/gatsby-monogram.svg').toString()
+    )
+    expect(result.relativePath).toContain('mocked.svg')
+  })
+  test('skip non-svg DatoCMS assets', async () => {
+    createResolvers({
+      cache,
+      createResolvers: createResolversMock,
+      store,
+      reporter
+    })
+    const resolverData = registeredResolvers.get('DatoCmsAsset')
+
+    const noSvgFixture = JSON.parse(JSON.stringify(datoCmsAssetFixture))
+    noSvgFixture.entityPayload.attributes.mime_type = 'no/svg'
+    const result = await resolverData.svg.resolve(noSvgFixture)
+
+    expect(result).toBe(null)
+    expect(reporter.panic).not.toHaveBeenCalled()
+  })
+
+  test('skip empty DatoCMS assets', async () => {
+    createResolvers({
+      cache,
+      createResolvers: createResolversMock,
+      store,
+      reporter
+    })
+    const resolverData = registeredResolvers.get('DatoCmsAsset')
+
+    const noFileFixture = JSON.parse(JSON.stringify(datoCmsAssetFixture))
+    delete noFileFixture.entityPayload.attributes.url
     const result = await resolverData.svg.resolve(noFileFixture)
 
     expect(result).toBe(null)
